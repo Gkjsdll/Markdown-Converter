7b3f3bff9abaf8547b620ffda6fe6e7e88a57ab5
Zack Winchell <zackwinchell@gmail.com>
2016-01-22 18:51:42 -0800

    made it pretty

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,22 @@
 "use strict";
 
-var $goConvert, $mdInput;
+var $goConvert, $mdInput, $htmlOutput;
 
 $(document).ready(init);
 
 function init(){
   $('#goConvert').click(convertMD);
   $mdInput = $('#mdInput');
+  $htmlOutput = $('#htmlOutput');
 }
 
 function convertMD(){
-  console.log($mdInput.val());
+  $.post("/markdown", {"Text":$mdInput.val()})
+  .success(function(data){
+    console.log("Success! Now do a thing!");
+    $htmlOutput.empty().append(data);
+  })
+  .fail(function(err){
+    console.log("Failure! Fix your shit!");
+  })
 }
--- routes/markdown.js
+++ routes/markdown.js
@@ -1,13 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var markdown = express.Router();
+var marked = require('marked');
 
 markdown.post("/", function(req, res){
-
+  res.send(marked(req.body.Text))
 });
-
-markdown.get("/", function(req, res){
-
-})
 
 module.exports = markdown;
